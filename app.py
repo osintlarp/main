4959d5da6c44253d414fc242e2ec2acf29b9b947
osintlarp <csintlarp@gmail.com>
2025-11-01 19:17:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
+
+def load_endpoints():
+    with open('endpoints.json', 'r') as f:
+        return json.load(f)
 
 @app.route("/")
 def home():
@@ -9,6 +14,11 @@ def home():
 @app.route("/dash")
 def dasg():
     return render_template("dashboard.html")
+
+@app.route('/v1/api_endpoints', methods=['GET'])
+def api_endpoints():
+    endpoints = load_endpoints()
+    return jsonify(endpoints)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
