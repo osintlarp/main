739ad8965a1594bf3095360b3c89d67f804ecd01
osintlarp <csintlarp@gmail.com>
2025-11-01 01:57:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/dash")
+def home():
+    return render_template("dashboard.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
