4297be42aeaa647e5ca974c55bcbd37846ddb5ec
osintlarp <csintlarp@gmail.com>
2025-11-14 18:01:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, send_file, request, abort
+from flask import Flask, render_template, jsonify, send_file, redirect, request, abort
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 from datetime import datetime
